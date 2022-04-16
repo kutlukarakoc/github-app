c4ca4e3152af2ac6348565bdc825e185f2b7dd7d
Kutlu Karakoc <89937328+kutlukarakoc@users.noreply.github.com>
2022-04-17 02:22:49 +0300

    Update and rename ui.js to toggle.js

---
+++ toggle.js
@@ -0,0 +1,42 @@
+const toggle = document.querySelector('.checkbox');
+const search_input = document.querySelector('.search');
+const search_btn = document.querySelector('.search-btn');
+const card_container = document.querySelector('.card-container');
+const avatar = document.querySelector('.avatar');
+const anchors = document.querySelectorAll('a');
+
+toggle.addEventListener('change', ()=> {
+    document.body.classList.toggle('dark-mode');
+    
+    if(search_input){
+        search_input.classList.toggle('search-darkmode');
+    };
+    
+    if(search_btn){
+        search_btn.classList.toggle('search-btn-darkmode');
+    };
+    
+    if(card_container){
+        card_container.classList.toggle('card-darkmode');
+    };
+    
+    if(avatar){
+        avatar.classList.toggle('avatar-darkmode');
+    };
+    
+    if(anchors){
+        anchors.forEach((element)=> {
+            element.classList.toggle('user-darkmode');
+        });
+    };
+    
+    if(followers || following){//can not find el probably, need to check
+        followers.classList.toggle('followers-darkmode');
+        following.classList.toggle('following-darkmode');
+    };
+    
+    if(github){//can not find el probably, need to check
+        github.classList.toggle('user-darkmode');
+    };
+
+});
