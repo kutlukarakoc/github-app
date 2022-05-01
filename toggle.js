4f9c73864d329c4aaaac9becc6877efee3f374c5
Kutlu Karakoc <89937328+kutlukarakoc@users.noreply.github.com>
2022-05-01 05:07:28 +0300

    Update toggle.js

--- toggle.js
+++ toggle.js
@@ -48,5 +48,77 @@ toggle.addEventListener('change', ()=> {
     if(github_user){
         github_user.classList.toggle('github-darkmode');
     };
-
 });
+
+function toggleDoubleCheck() { // to fix toggle bugs
+    if (document.body.classList[0] == 'dark-mode' && checkbox.checked) {
+        if (avatar) {
+            if (avatar.length == 1) {
+                if (!avatar[0].classList.contains('avatar-darkmode')) {
+                    avatar[0].classList.add('avatar-darkmode');
+                };
+            } else {
+                avatar.forEach((element) => {
+                    if (!element.classList.contains('avatar-darkmode')) {
+                        element.classList.add('avatar-darkmode');
+                    };
+                });
+            }
+        };
+
+        if (anchors) {
+            if (anchors.length == 1) {
+                if (!anchors[0].classList.contains('user-darkmode')) {
+                    anchors[0].classList.add('user-darkmode');
+                };
+            } else {
+                anchors.forEach((element) => {
+                    if (!element.classList.contains('user-darkmode')) {
+                        element.classList.add('user-darkmode');
+                    };
+                });
+            }
+        };
+
+        if(user_followers.style.display != 'block' && !user_followings.style.display != 'block'){
+            if(document.querySelectorAll('.user').length){ // github_user
+                if(!github_user.classList.contains('github-darkmode')){
+                    github_user.classList.add('github-darkmode');
+                };
+            };
+
+            if(document.querySelectorAll('.followers').length){ // followers
+                if(!followers.classList.contains('followers-darkmode')){
+                    followers.classList.add('followers-darkmode');
+                };
+            };
+
+            if(document.querySelectorAll('.following').length){ // following
+                if(!following.classList.contains('following-darkmode')){
+                    following.classList.add('following-darkmode');
+                };
+            };
+        };
+
+    }else{
+        if(user_followers.style.display != 'block' && user_followings.style.display != 'block'){ // if searched user displays
+            if(document.querySelectorAll('.user').length){ // github_user
+                if(github_user.classList.contains('github-darkmode')){
+                    github_user.classList.remove('github-darkmode');
+                };
+            };
+
+            if(document.querySelectorAll('.followers').length){ // followers
+                if(followers.classList.contains('followers-darkmode')){
+                    followers.classList.remove('followers-darkmode');
+                };
+            };
+
+            if(document.querySelectorAll('.following').length){ // following
+                if(following.classList.contains('following-darkmode')){
+                    following.classList.remove('following-darkmode');
+                };
+            };
+        };
+    }
+};
